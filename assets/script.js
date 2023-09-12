f3f20e2a5e8af739b339a34b498400d0801d1a76
thanhdanh27 <maithanhdanh112@gmail.com>
2023-09-12 14:32:38 +0700

    finish

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,25 @@ window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+
+const menu = document.querySelector('.mobile-bar')
+const nav = document.querySelector('.navbar')
+menu.onclick = function () {
+    var isClose = nav.clientHeight === 136;
+    if (isClose) {
+        nav.classList.remove('open')
+    }
+    else {
+        nav.classList.add('open')
+    }
+}
+
+const menuItems = document.querySelectorAll('.navbar__item')
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.onclick = function () {
+        nav.classList.remove('open')
+    }
+}
